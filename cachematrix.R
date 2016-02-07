a9baebc09f84fb0850e41a9546a049c37d37bce6
aksrathore <abhinavrathod@gmail.com>
2016-02-07 18:35:13 +0530

    Intial Commit to cachematrix.R for programming assignment of Coursera R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,46 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Inverse matrices are stored in the memory using scoping rules
 makeCacheMatrix <- function(x = matrix()) {
+		matrixInverse <- NULL
+		set <- function(y) {
+			x <<- y
+			matrixInverse <- NULL
+		}
+		get <- function() x
+		setInverse <- function(Inverse) matrixInverse <<- Inverse
+		getInverse <- function() matrixInverse
+		list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 
 }
 
-
 ## Write a short comment describing this function
-
+## Function to return the matrix inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Here corcopr librarary is used to generate inverse of the matrix 
+		## to avoid singularity error
+		if(require("corpcor")){
+			print("corpcor library is already loaded")
+		} else {
+		print("Installing corpcor library")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("corpcor installed and loaded")
+			} else {
+				stop("could not install corpcor")
+			}
+		}
+		
+		## Return a matrix that is the inverse of 'x'
+		inverse <- x$getInverse()
+		if(!is.null(inverse)){
+			message("getting cached matrix from the memory")
+		return(inverse)
+		}
+		message("The inverse of the matrix is not in memory so the inverse is computed")
+		data <- x$get()
+		inverse <- pseudoinverse(data, ...)
+		x$setInverse(inverse)
+		inverse
 }
